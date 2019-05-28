ff75d408f04eb26c717cdc481235f76915328eff
Vítor Meira <a11554@alunos.ipca.pt>
2019-05-28 16:31:05 +0100

    VAO e VBO

--- ProjectP3D/Source.cpp
+++ ProjectP3D/Source.cpp
@@ -8,6 +8,8 @@
 #include <sstream>
 #include <fstream>
 
+#define GLFW_USE_DWM_SWAP_INTERVAL
+
 #define GLEW_STATIC
 #pragma region Includes GL, GLFW, GLM
 #include <GL\glew.h>
@@ -16,11 +18,16 @@
 #include <glm/gtc/matrix_transform.hpp> 
 #include <glm/gtc/type_ptr.hpp> 
 #pragma endregion
-
+#include "LoadShaders.h"
 void init(void);
 
 #define WIDTH 800
 #define HEIGHT 600
+
+#define NumVAOs 1
+#define NumBuffers 3 // Vértices, Cores
+GLuint VAOs[NumVAOs];
+GLuint Buffers[NumBuffers];
 
 GLfloat ZOOM = 10.0f;
 
@@ -137,6 +144,21 @@ void loadOBJ(const char *path, std::vector <glm::vec3> &out_vertices, std::vecto
 			normalIndices.push_back(normalIndex[1]);
 			normalIndices.push_back(normalIndex[2]);
 		}
+
+		for (unsigned int i = 0; i < vertexIndices.size(); i++) {
+			
+			unsigned int vertexIndex = vertexIndices[i];
+			glm::vec3 vertex = temp_vertices[vertexIndex - 1];
+			out_vertices.push_back(vertex);
+
+			unsigned int textureIndex = textureIndices[i];
+			glm::vec2 text = temp_text[textureIndex - 1];
+			out_textures.push_back(text);
+
+			unsigned int normalIndex = normalIndices[i];
+			glm::vec3 normal = temp_normals[normalIndex - 1];
+			out_normals.push_back(normal);
+		}
 	}
 }
 
@@ -211,8 +233,111 @@ void showCube(std::vector<glm::vec3> obj, glm::mat4 mvp) {
 	glEnd();
 }
 
+void init_two_VBO_Packed_Arrays(std::vector <glm::vec3> vertices, 
+	std::vector <glm::vec2> &textures, std::vector <glm::vec3> &normals) {
+	// ****************************************************
+	// VAOs - Vertex Array Objects
+	// ****************************************************
+
+	// Gerar 'NumVAOs' nomes para VAOs.
+	// Neste caso, como NumVAOS = 1, gera apenas 1 nome.
+	glGenVertexArrays(NumVAOs, VAOs);
+	// Faz bind do VAO, cujo nome está definido em 'VAOs[0]', com o contexto do OpenGL.
+	// Um VAO é criado no primero bind que lhe seja feito.
+	// Este VAO passa a estar ativo até que seja feito o bind a outro VAO, ou seja feito o bind com valor 0.
+	glBindVertexArray(VAOs[0]);
+
+	// ****************************************************
+	// VBOs - Vertex Buffer Objects
+	// ****************************************************
+
+	// Gera 'NumBuffers' nomes para VBOs.
+	// Neste caso, como NumBuffers = 2, gera 2 nomes
+	// Esta função pode ser chamada antes da criação de VAOs.
+	glGenBuffers(NumBuffers, Buffers);
+	// Para cada nome de VBO
+	for (int i = 0; i < NumBuffers; i++) {
+		// Faz bind do VBO ao buffer GL_ARRAY_BUFFER.
+		// GL_ARRAY_BUFFER é utilizado para dados de atributos de vértices.
+		// Um VBO é criado no primeiro bind que lhe seja feito.
+		// Este VBO passa a estar ativo até que seja feito o bind a outro VBO ou seja feito o bind com valor 0.
+		// Para alocar a memória para os BO, não é necessário que já esteja vinculado um VAO.
+		glBindBuffer(GL_ARRAY_BUFFER, Buffers[i]);
+		if (i == 0) {
+			// Inicializa o VBO (que está ativo) com memória de tamanho imutável.
+			glBufferStorage(GL_ARRAY_BUFFER, sizeof(vertices) /*2 * 6 * sizeof(float)*/, &vertices.front(), 0);
+			// Inicializa o VBO (que está ativo) com memória de tamanho mutável.
+			//glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		}
+		else if (i == 1) {
+			// Inicializa o VBO (que está ativo) com memória de tamanho imutável.
+			glBufferStorage(GL_ARRAY_BUFFER, sizeof(textures) /*3 * 6 * sizeof(float)*/, &textures.front(), 0);
+			// Inicializa o VBO (que está ativo) com memória de tamanho mutável.
+			//glBufferData(GL_ARRAY_BUFFER, sizeof(cores), vertices, GL_STATIC_DRAW);
+		}
+		else {
+			glBufferStorage(GL_ARRAY_BUFFER, sizeof(normals) /*2 * 6 * sizeof(float)*/, &normals.front(), 0);
+		}
+	}
+
+	// ****************************************************
+	// Shaders
+	// ****************************************************
+
+	ShaderInfo  shaders[] = {
+		{ GL_VERTEX_SHADER,   "triangles.vert" },
+		{ GL_FRAGMENT_SHADER, "triangles.frag" },
+		{ GL_NONE, NULL }
+	};
+
+	GLuint programa = LoadShaders(shaders);
+	if (!programa) exit(EXIT_FAILURE);
+	glUseProgram(programa);
+
+	// ****************************************************
+	// Ligar Atributos aos Shaders
+	// ****************************************************
+
+	// As localizações de atributos fazem parte do programa shader, e são completamente independentes do VAO.
+	// Podemos, assim, obter as localizações de atributos sem que antes se tenha criado qualquer VAO.
+	// ------------------------------------------------------------------------------------------------------
+	// Obtém a localização do atributo 'vPosition' no 'programa'.
+	//GLint coordsId = glGetAttribLocation(programa, "vPosition"); // Para versões anteriores à 4.3
+	GLint coordsId = glGetProgramResourceLocation(programa, GL_PROGRAM_INPUT, "vPosition"); // Para versões >= 4.3
+	// Obtém a localização do atributo 'vColors' no 'programa'.
+	//GLint coresId = glGetAttribLocation(programa, "vColors"); // Para versões anteriores à 4.3
+	GLint coresId = glGetProgramResourceLocation(programa, GL_PROGRAM_INPUT, "vColors"); // Para versões >= 4.3
+
+	//glBindVertexArray(VAOs[0]); // Não é necessário fazer o bind do VAO, pois ele já é o que está ativo no contexto do OpenGL.
+
+	// Ativa o VBO 'Buffers[0]'.
+	glBindBuffer(GL_ARRAY_BUFFER, Buffers[0]);
+	// Liga a localização do atributo 'vPosition' dos shaders do 'programa', ao VBO e VAO (ativos).
+	// Aqui já é necessário ter o VAO e o VBO vinculados ao contexto do OpenGL.
+	// É neste ponto que o VBO fica associado ao VAO.
+	// Especifica também como é que a informação do atributo 'coordsId' deve ser interpretada.
+	// Neste caso, o atributo irá receber, por vértice, 2 elementos do tipo float.
+	glVertexAttribPointer(coordsId, 2 /*2 elementos por vértice*/, GL_FLOAT/*do tipo float*/, GL_FALSE, 0, nullptr);
+
+	// Ativa o VBO 'Buffers[1]'.
+	glBindBuffer(GL_ARRAY_BUFFER, Buffers[1]);
+	// Liga a localização do atributo 'vColors' dos shaders do 'programa', ao VBO e VAO (ativos).
+	// Aqui já é necessário ter o VAO e o VBO vinculados ao contexto do OpenGL.
+	// É neste ponto que o VBO fica associado ao VAO.
+	// Especifica também como é que a informação do atributo 'coordsId' deve ser interpretada.
+	// Neste caso, o atributo irá receber, por vértice, 3 elementos do tipo float.
+	glVertexAttribPointer(coresId, 3 /*3 elementos por vértice*/, GL_FLOAT/*do tipo float*/, GL_FALSE, 0, nullptr);
+
+	// Habitita o atributo com localização 'coresId' para o VAO ativo.
+	// Os atributos de um VAO estão desativados por defeito.
+	glEnableVertexAttribArray(coordsId);
+	// Habitita o atributo com localização 'coresId' para o VAO ativo.
+	// Os atributos de um VAO estão desativados por defeito.
+	glEnableVertexAttribArray(coresId);
+}
+
 int main(void) {
-	std::vector<glm::vec3> obj = Load3DModel();
+	//std::vector<glm::vec3> obj = Load3DModel();
 
 	GLFWwindow *window;
 
@@ -237,10 +362,10 @@ int main(void) {
 	glfwSetScrollCallback(window, scrollCallback);
 
 	char path[128] = "C:\\Users\\VitorHugodeCastroeMe\\Desktop\\P3D-TP\\Iron_Man\\Iron_Man.obj";
-	std::vector <glm::vec3> out_vertices;
-	std::vector <glm::vec2> out_textures;
-	std::vector <glm::vec3> out_normals;
-	loadOBJ(path, out_vertices, out_textures, out_normals);
+	std::vector <glm::vec3> vertices;
+	std::vector <glm::vec2> textures;
+	std::vector <glm::vec3> normals;
+	loadOBJ(path, vertices, textures, normals);
 
 	// Projection
 	glm::mat4 projection = glm::perspective(glm::radians(45.0f), float(WIDTH) / float(HEIGHT), 0.1f, 100.f);
@@ -262,7 +387,9 @@ int main(void) {
 		// MVP
 		glm::mat4 mvp = projection * view * model;
 
-		showCube(obj, mvp);
+		//showCube(obj, mvp);
+		glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
+
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
